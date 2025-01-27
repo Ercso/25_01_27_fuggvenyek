87f76dc78b0cad2e3b54c19a07f5f637e63b355b
Szalkay Erik <szalkay.erik@diak.szbi-pg.hu>
2025-01-27 08:46:38 +0100

    update

---
+++ 03_eljaras_listaval.py
@@ -0,0 +1,13 @@
+
+def osszegzo(list):
+	osszesen = 0
+	# for szam in list:
+	# 	osszesen = osszesen + szam
+    osszesen = sum(lista)
+	print('A listában lévő számok összege: ', osszesen)
+
+
+szamok = [3, 5, 19, 11, 17, 1]
+osszegzo(szamok)
+    
+    
